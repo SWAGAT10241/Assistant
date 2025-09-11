da6d15f66b0b415f999a34a08fe354d692d50890
SWAGAT SOURAV DHAR <swagatsourav10241@gmail.com>
2025-09-11 12:07:08 +0530

    new commit

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { signUp, Login, LogOut } from "../controllers/auth.controllers.js";
+
+const authRouter = express.Router();
+
+authRouter.post("/signup", signUp);
+authRouter.post("/signin", Login);
+authRouter.get("/logout", LogOut);
+
+
+export default authRouter;
