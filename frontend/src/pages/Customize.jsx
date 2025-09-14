96911555014fe77332b46d51dcd79d7d75fb5c53
SWAGAT SOURAV DHAR <swagatsourav10241@gmail.com>
2025-09-14 12:32:47 +0530

    Fix error Customize.jsx

--- frontend/src/pages/Customize.jsx
+++ frontend/src/pages/Customize.jsx
@@ -33,7 +33,7 @@ function Customize() {
         <Card image={image5} />
         <Card image={image6} />
         <Card image={image7} />
-        <div className={`w-[70px] h-[140px] lg:w-[150px] lg:h-[250px] bg-[#020220] border-2 border-[#0000ff66] rounded-2xl overflow-hidden hover:shadow-2xl hover:shadow-blue-950 cursor-pointer hover:border-4 hover:border-white flex items-center justify-center ${selectedImage === "input" ? "border-4 border-white shadow-2xl shadow-blue-950" : "null"}`} onClick={() => {
+        <div className={`w-[70px] h-[140px] lg:w-[150px] lg:h-[250px] bg-[#020220] border-2 border-[#0000ff66] rounded-2xl overflow-hidden hover:shadow-2xl hover:shadow-blue-950 cursor-pointer hover:border-4 hover:border-white flex items-center justify-center ${selectedImage === "input" ? "border-4 border-white shadow-2xl shadow-blue-950" : ""}`} onClick={() => {
           inputImage.current.click()
           setSelectedImage("input")
         }}>
